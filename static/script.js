839594a8e283917e9b42d011c71f68b0e50d9b5d
Blake Martinez <blakemartinez@Blakes-MBP-2.attlocal.net>
2023-08-30 23:11:02 -0500

    collapse feature

--- static/script.js
+++ static/script.js
@@ -4,3 +4,23 @@ const readCount = document.querySelectorAll('.done .book-entry').length;
 // Update the book count in the HTML
 const readCountElement = document.getElementById('read-count');
 readCountElement.textContent = readCount;
+
+const lastUpdatedElement = document.getElementById('last-updated');
+
+const lastUpdatedTimestamp = new Date('2023-09-01T22:02:45').getTime(); // Replace with time
+
+const formattedDate = new Date(lastUpdatedTimestamp).toLocaleString('en-US', {
+    timeZone: 'America/Chicago', 
+});
+
+const toggleIcons = document.querySelectorAll('.toggle-icon');
+
+toggleIcons.forEach((toggleIcon) => {
+    toggleIcon.addEventListener('click', () => {
+        console.log("yo");
+        const section = toggleIcon.closest('.book-section');
+        const sectionContent = section.querySelector('.section-content');
+        sectionContent.classList.toggle('expanded');
+        sectionContent.classList.toggle('collapsed');
+    });
+});
